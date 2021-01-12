ed39b0edbcb7293df677e1022b84c3c5b489aad7
Marina <artyusmarina@yandex.ru>
2021-01-12 22:56:48 +0300

    init: start react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
